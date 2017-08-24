50bca43113b40fef6a4dbbf168ea86d0beaeb016
SlawekPelka <s.pelka@live.nl>
2017-08-24 17:05:51 +0200

    :pencil: refactor handler file, add copy x times validator

--- src/lib/helpers/handler.ts
+++ src/lib/helpers/handler.ts
@@ -1,3 +1,5 @@
+import gencodes from './generateCodes';
+
 export default class Handler {
 
     private filiaal: any;
@@ -14,9 +16,32 @@ export default class Handler {
 
     private validate() {
         if (!this.xtimesCheck.test(this.xtimes.value)) {
-            document.querySelector('.container').insertAdjacentHTML('afterbegin', '<div class="alert alert-dismissible alert-danger"><button type="button" class="close" data-dismiss="alert">&times;</button>Copy x times fields accepts only numbers (sparated by comma).</div>');
-            this.filiaal.offsetParent.className += " has-error";
-            this.filiaal.className += " form-control";
+            document.querySelector('.container').insertAdjacentHTML('afterbegin', '<div class="alert alert-danger">Copy x times field accepts only numbers (sparated by comma).</div>');
+            this.xtimes.offsetParent.className += " has-error";
+        } else {
+            this.xtimes.offsetParent.classList.remove("has-error");
+            this.generateCodes();
+        }
+    }
+
+    private splitInput() {
+        let splFiliaal: string = this.filiaal.value.split(',');
+        let splXtimes: string = this.xtimes.value.split(',');
+
+        this.filiaal = splFiliaal;
+        this.xtimes = splXtimes;
+    }
+
+    private generateCodes() {
+        this.splitInput();
+
+        try {
+            for (let i: number = 0; i < this.xtimes.length; i++) {
+                let generated: any = new gencodes(this.filiaal[i], this.xtimes[i]);
+                generated.download(generated.codes());
+            }
+        } catch (e) {
+            console.error(e);
         }
     }
 }
